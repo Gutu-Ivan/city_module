9ee31ead4c5d175b12371b95e980696257f092ba
Gutu Ivan <gutsu.ivan@mail.ru>
2021-09-20 18:07:14 +0300

    Initial commit

---
+++ src/main/java/dev/gutuivan/CityModuleApplication.java
@@ -0,0 +1,13 @@
+package dev.gutuivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gutuivan/CityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gutuivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
