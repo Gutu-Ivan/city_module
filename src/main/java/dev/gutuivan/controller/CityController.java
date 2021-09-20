6ca21e80155a35a8244d0f78244f1f5729d6274f
Gutu Ivan <gutsu.ivan@mail.ru>
2021-09-20 20:04:41 +0300

    Added: +CRUD methods into CitiesRepository Updated: * createCity method into CityController

--- src/main/java/dev/gutuivan/controller/CityController.java
+++ src/main/java/dev/gutuivan/controller/CityController.java
@@ -1,15 +1,17 @@
 package dev.gutuivan.controller;
 
+import dev.gutuivan.repository.CitiesRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 
 public class CityController {
+    CitiesRepository citiesRepository = new CitiesRepository();
 
     @PostMapping("cities")
-    public String createCity(){
-        return "asd";
+    public String createCity(@RequestParam String cityName){
+        return citiesRepository.createCity(cityName);
     }
     @GetMapping("cities")
     public String readCity(){
--- src/main/java/dev/gutuivan/repository/CitiesRepository.java
+++ src/main/java/dev/gutuivan/repository/CitiesRepository.java
@@ -1,4 +1,30 @@
 package dev.gutuivan.repository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CitiesRepository {
+
+    String[] cities = {"Balti", "Chisinau", "Orhei", "Cahul"};
+    List<String> citiesList = Arrays.asList(cities);
+
+
+    public String createCity(String cityName){
+        this.citiesList.add(cityName);
+        return cityName + "is created";
+    }
+    public String readCity(){
+        return "";
+    }
+    public String updateCity(){
+        return "";
+    }
+    public String deleteCity(){
+        return "";
+    }
+
+
+
+
 }
