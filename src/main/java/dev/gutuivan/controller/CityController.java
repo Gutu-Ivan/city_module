0dcbd839c61c8cbe2d5845efa082e0846fee11b4
Gutu Ivan <gutsu.ivan@mail.ru>
2021-09-20 18:42:17 +0300

    Added: +Controller +Repository +CRUD methods into CityController

---
+++ src/main/java/dev/gutuivan/controller/CityController.java
@@ -0,0 +1,19 @@
+package dev.gutuivan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityController {
+    public String createCity(){
+        return "";
+    }
+    public String readCity(){
+        return "";
+    }
+    public String readAllCities(){
+        return "";
+    }
+    public String DeleteCity(){
+        return "";
+    }
+}
---
+++ src/main/java/dev/gutuivan/repository/CitiesRepository.java
@@ -0,0 +1,4 @@
+package dev.gutuivan.repository;
+
+public class CitiesRepository {
+}
