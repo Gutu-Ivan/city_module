96ff04061855f43d99d6ff5d4ecc172c86a8a0fb
Gutu Ivan <gutsu.ivan@mail.ru>
2021-09-23 22:04:18 +0300

    Added: +CountriesRepository +CountryController +CountryModel

---
+++ src/main/java/dev/gutuivan/controller/CountryController.java
@@ -0,0 +1,32 @@
+package dev.gutuivan.controller;
+
+import dev.gutuivan.repository.CountriesRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+
+public class CountryController {
+    CountriesRepository countriesRepository = new CountriesRepository();
+
+    @PostMapping("countries")
+    public String createCountry(@RequestParam String countryName){
+        return countriesRepository.createCountry(countryName);
+    }
+    @GetMapping("countries/{id}")
+    public String readCountry(@PathVariable Integer id){
+        return countriesRepository.readCountry(id);
+    }
+    @GetMapping("countries/all")
+    public String readAllCountries(){
+        return countriesRepository.readAllCountries();
+    }
+    @PutMapping("countries/{id}")
+    public String updateCountry(@PathVariable Integer id, @RequestParam String countryName){
+        return countriesRepository.updateCountry(id, countryName);
+    }
+    @DeleteMapping("countries/{id}")
+    public String deleteCountry(@PathVariable Integer id){
+        return countriesRepository.deleteCountry(id);
+    }
+}
---
+++ src/main/java/dev/gutuivan/repository/CountriesRepository.java
@@ -0,0 +1,54 @@
+package dev.gutuivan.repository;
+
+import dev.gutuivan.model.CountryModel;
+
+import java.util.ArrayList;
+
+public class CountriesRepository {
+
+    ArrayList<CountryModel> countries = new ArrayList<>();
+
+
+    public String createCountry(String countryName){
+        CountryModel country = new CountryModel(countryName);
+        this.countries.add(country);
+        return String.valueOf(this.countries);
+    }
+    public String readCountry(Integer id){
+        try{
+            CountryModel country = this.countries.stream().filter(element -> id.equals(element.getId())).findFirst().orElse(null);
+            return country.toString();
+        }catch (Exception exception){
+            return "Not found";
+        }
+    }
+    public String readAllCountries(){
+        return this.countries.toString();
+    }
+
+    public String updateCountry(Integer id, String countryName){
+        try{
+            CountryModel country = this.countries.stream().filter(element -> id.equals(element.getId())).findFirst().orElse(null);
+            country.setName(countryName);
+            return "Country updated";
+        }catch (Exception exception){
+            return "Not found";
+        }
+    }
+    public String deleteCountry(Integer id){
+        try{
+            CountryModel country = this.countries.stream().filter(element -> id.equals(element.getId())).findFirst().orElse(null);
+            if (country == null){
+                return "Country not found";
+            }
+            this.countries.remove(country);
+            return "Country deleted";
+        }catch (Exception exception){
+            return "Not found";
+        }
+    }
+
+
+
+
+}
